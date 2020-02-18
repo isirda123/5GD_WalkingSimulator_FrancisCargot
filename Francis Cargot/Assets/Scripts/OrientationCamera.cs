45370e5aa1f00ca1f7f0a9446db0ffce13f2ad2a
Isirda123 <isirda123@yahoo.fr>
2020-02-18 16:22:58 +0100

    Movement Camera

---
+++ Francis Cargot/Assets/Scripts/OrientationCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrientationCamera : MonoBehaviour
+{
+    Vector2 mouseLastPos;
+    Vector2 mousePos;
+    [SerializeField] float sensibilityOfMouth;
+    [SerializeField] float xRotationMax;
+    [SerializeField] float yRotationMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mouseLastPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtTheMouse();
+    }
+
+    void LookAtTheMouse()
+    {
+        mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 mouvementOfMouse = mousePos - mouseLastPos;
+        this.transform.Rotate(-mouvementOfMouse.y * sensibilityOfMouth, mouvementOfMouse.x * sensibilityOfMouth,0);
+        this.transform.rotation = Quaternion.Euler(Mathf.Clamp(this.transform.rotation.eulerAngles.x, -xRotationMax, xRotationMax), 
+            Mathf.Clamp(this.transform.rotation.eulerAngles.y, -yRotationMax, yRotationMax), this.transform.rotation.eulerAngles.z);
+
+        mouseLastPos = mousePos;
+    }
+}
