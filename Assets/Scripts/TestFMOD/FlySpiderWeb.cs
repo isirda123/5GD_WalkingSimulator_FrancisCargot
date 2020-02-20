3ce3cecb8ebc9bf83e7225bdef256cc3a4977e9a
Clement <clement.roth@live.fr>
2020-02-20 16:52:46 +0100

    WIP intégration son mouche / araignée / fourmi

---
+++ Assets/Scripts/TestFMOD/FlySpiderWeb.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlySpiderWeb : MonoBehaviour
+{
+    public Transform[] positionTarget;
+
+    public FMODUnity.StudioEventEmitter fly_event;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(3f, 10f));
+
+            fly_event.transform.position = positionTarget[Random.Range(0, positionTarget.Length)].position;
+            fly_event.Play();
+        }
+    }
+}
